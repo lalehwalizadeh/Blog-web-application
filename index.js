edee990d8083fd3a04c303ebd9192d3d82da679c
LalehWalizadeh <lalah.walizadah2022@gmail.com>
2024-05-09 14:07:02 +0330

    commit

--- index.js
+++ index.js
@@ -47,16 +47,40 @@ const year = date.getFullYear()
 // define an Array as database:
 let posts = [
   {
-    title: "butterfly",
-    description: "Buttreflies are facinating creatures symbolizing tranformation, beauty, and tranquility.",
-    picture: "/images/butterfly.jpg",
+    title: "The Keeper of Stars",
+    description: "'Keeper of Stars' is a heartwarming tale of love, loss, and redemption set against the backdrop of a small town in the mountains. The story follows a young woman who inherits her grandmother's old bookstore and discovers a hidden journal that reveals secrets from the past. As she delves deeper into the mysteries of her family history, she uncovers a connection to the stars that changes her life forever",
+    picture: "/images/the keeper.jpg",
     id: 1,
   },
   {
-    title: "Dark",
-    description: "The dark night sky envelops the world in a blanket of mystery and intrigue.",
-    picture: "/images/dark.jpg",
+    title: "You Become What You Think",
+    description: "'You Become What You Think' is a powerful self-help book that explores the connection between our thoughts and our reality. Through practical exercises and insightful guidance, the author shows readers how to harness the power of positive thinking to transform their lives. This book serves as a roadmap for creating a mindset that aligns with one's goals and aspirations",
+    picture: "/images/you become what you think.jpg",
     id: 2,
+  },
+  {
+    title: "You Better Be Lighting",
+    description: "'You Better Be Lighting' is a captivating novel that explores the complexities of love, loss, and redemption. With vivid imagery and compelling characters, this book will keep you on the edge of your seat until the very last page. A must-read for anyone who enjoys a thought-provoking and emotionally charged story",
+    picture: "/images/you better be.jpg",
+    id: 3,
+  },
+  {
+    title: "Where the Forest Meets the Stars",
+    description: "'Where the Forest Meets the Stars' is a captivating tale of love, loss, and redemption set in the tranquil beauty of a remote forest. The story follows a young girl who befriends a mysterious woman claiming to be an alien, leading to unexpected discoveries and profound connections. With lyrical prose and rich character development, this novel explores themes of healing and hope in the face of adversity",
+    picture: "/images/forest meets the stars.jpg",
+    id: 4,
+  },
+  {
+    title: "Five Feet Apart",
+    description: "'Five Feet Apart' is a heart-wrenching novel about two teenagers with cystic fibrosis who fall in love while being forced to stay five feet apart to prevent cross-infection. The emotional rollercoaster of their forbidden romance will keep readers on the edge of their seats until the very end. This poignant story explores themes of love, loss, and the power of human connection in the face of adversity",
+    picture: "/images/five feet.jpg",
+    id: 5,
+  },
+  {
+    title: "Edge os Madness",
+    description: " 'Edge of Madness' is a gripping psychological thriller that will keep you on the edge of your seat.With its intricate plot twists and complex characters, this book delves into the dark depths of the human mind.  Prepare to be taken on a thrilling journey to the edge of madness in this chilling and suspenseful novel.",
+    picture: "/images/edge of madness.jpg",
+    id: 6,
   },
  
 ]
@@ -114,8 +138,8 @@ app.put("/update/:id", upload.single("image"), (req, res) => {
   const postId = parseInt(req.params.id)
   const updatedPostIndex = posts.findIndex((p) => p.id == postId)
 
-  const pic_path = req.file.path
-  const imageReplacement = pic_path.replace("public\\", "")
+  const pic_path = req.file ? "/images" + req.file.filename : null;
+
 
   if (updatedPostIndex === -1) {
     return res.status(404).send("post not found")
@@ -124,10 +148,11 @@ app.put("/update/:id", upload.single("image"), (req, res) => {
     id: postId,
     title: req.body.title,
     description: req.body.description,
-    picture: imageReplacement,
+    picture: pic_path,
   }
 
   posts[updatedPostIndex] = updatedPost
+ 
   res.redirect("/")
  
 })
